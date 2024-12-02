92c52808af49f5a10d1508d8b3e38e448fee8372
Nastya231213 <nastya1200526@gmail.com>
2024-12-02 11:33:07 +0200

    Add .env file for DSN configuration

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"log"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,7 +14,16 @@ var (
 )
 
 func Connect() {
-	dsn := ""
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Error loading .env file: %v\n", err)
+	}
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		log.Fatal("DB_DSN environment variable not set")
+	}
+
 	d, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
